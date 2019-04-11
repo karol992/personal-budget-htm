b93773495547fabbe4b73ce31445f3523284ff4b
Karol Sołek <karol.solek.programista@gmail.com>
2019-04-11 22:46:31 +0200

    pie-chart-added

--- js/piechart.js
+++ js/piechart.js
@@ -1,22 +1,59 @@
-// Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+var jedzenie = 492.44;
+var mieszkanie = 1450.00;
+var transport = 153.87;
+var telekomunikacja = 142.79;
+var higiena = 0.00;
+var dzieci = 0.00;
+var rozrywka = 135.86;
+var wycieczka = 0.00;
+var szkolenia = 0.00;
+var ksiazki = 191.88;
+var oszczednosci = 0.00;
+var emerytura = 0.00;
+var dlugi = 0.00;
+var darowizna = 94.35;
+var inne = 652.76;
 
-// Draw the chart and set the chart values
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChartPieChart);
+function drawChartPieChart(){
+	var data = google.visualization.arrayToDataTable([
   ['Wydatek', 'Ilość'],
-  ['Jedzenie', 492.44],
-  ['Mieszkanie', 1450],
-  ['Transport', 153.87],
-  ['Telekomunikacja', 142.79],
-  ['Higiena', 0]
-]);
-
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'Wydatki', 'width':550, 'height':400};
-
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
+  ['Jedzenie', jedzenie],
+  ['Mieszkanie', mieszkanie],
+  ['Transport', transport],
+  ['Telekomunikacja', telekomunikacja],
+  ['Higiena', higiena],
+  ['Dzieci', dzieci],
+  ['Rozrywka', rozrywka],
+  ['Wycieczka', wycieczka],
+  ['Szkolenia', szkolenia],
+  ['Książki', ksiazki],
+  ['Oszczędności', oszczednosci],
+  ['Emerytura', emerytura],
+  ['Długi', dlugi],
+  ['Darowizna', darowizna],
+  ['Inne', inne]
+],false);
+	var options = {
+		"title":"Wykres kołowy wydatków",
+		"titleTextStyle":{"fontSize":"30"},
+		"width":"840",
+		"height":"790",
+		"chartArea":{"width":"840"},
+		"is3D":"true",
+		"pieSliceText":"percentage",
+		"pieSliceTextStyle":{"fontSize":"20"},
+		"legend":{
+			"alignment":"left",
+			"position":"top",
+			"maxLines":"5",
+			"textStyle":{
+				"fontSize":"27"
+			}
+		}
+	};
+	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+	chart.draw(data, options);
 }
+
